daf2bf800e1c995f4b94b051925b31ef8dc780fc
h3llsize <kirillbashkov2004@gmail.com>
2022-11-14 16:44:53 +0500

    Create project

---
+++ src/test/java/org/doomdev/bazanews/BazanewsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.doomdev.bazanews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazanewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
